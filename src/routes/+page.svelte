b1a5c082d27333150eb98ea297496ab4173fcf39
gabors0 <gabors0@proton.me>
2026-06-29 14:39:58 +0200

    feat: hide export cell

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,38 @@
-<div class="m-2 min-h-[calc(100dvh-1rem)] grid grid-cols-1 grid-rows-[3rem_auto_auto_auto] gap-x-3 gap-y-1 *:p-2 md:h-[calc(100dvh-1rem)] md:grid-cols-2 md:grid-rows-[3rem_3fr_2fr]">
+<script lang="ts">
+    let showExport = $state(true);
+</script>
+
+<div
+    class={[
+        'm-2 grid min-h-[calc(100dvh-1rem)] grid-cols-1 gap-x-3 gap-y-1 *:p-2 md:h-[calc(100dvh-1rem)] md:grid-cols-2',
+        showExport
+            ? 'grid-rows-[3rem_auto_auto_auto] md:grid-rows-[3rem_3fr_2fr]'
+            : 'grid-rows-[3rem_auto_auto_auto] md:grid-rows-[3rem_minmax(0,1fr)_auto]'
+    ]}
+>
     <div class="border-4 border-bg-dim text-accent md:col-span-2">fastfetch config generator</div>    
-    <fieldset class="">
+    <fieldset>
         <legend>Preview</legend>
         yo
     </fieldset>
     <fieldset class="md:row-span-2">
         <legend>Config</legend>
     </fieldset>
-    <fieldset class="">
-        <legend>Export</legend>
-        yo
+    <fieldset class="overflow-hidden">
+        <legend>
+            <button
+                type="button"
+                class="cursor-pointer text-accent-muted hover:underline hidden md:block"
+                aria-expanded={showExport}
+                onclick={() => (showExport = !showExport)}
+            >
+                Export -- {showExport ? "hide" : "show"}
+            </button>
+            <span class="md:hidden block">Export</span>
+        </legend>
+        {#if showExport}
+            yo
+        {/if}
     </fieldset>
 </div>
 
