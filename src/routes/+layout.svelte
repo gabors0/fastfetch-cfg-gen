394571172035c49832430215cd317eaf6d335569
gabors0 <gabors0@proton.me>
2026-06-29 13:42:02 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+{@render children()}
+
+<style>
+    @reference "/src/routes/layout.css";
+    :global(body) {
+        @apply bg-bg text-fg;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class="m-2 h-[calc(100dvh-1rem)] grid grid-cols-2 grid-rows-[3rem_1fr_1fr] gap-2 *:p-2">
+    <div class="col-span-2 border-4 border-bg-dim">d</div>
+    <div class=" border-4 border-bg-dim">yo</div>
+    <div class="row-span-2 border-4 border-bg-dim"></div>
+    <div class=" border-4 border-bg-dim">yo</div>
+</div>
